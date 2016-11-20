e64bb37232f0183bae5226cbc33e8080e211c536
marksayers45 <marksayers96@gmail.com>
2016-11-20 22:06:02 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,5 @@
 # Define the variable board below.
+var = "board"
+board = "array"
+board = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
